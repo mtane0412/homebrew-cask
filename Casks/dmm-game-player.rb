d9b3f1cea8d4e4e7961e420888379dda4747fe61
種延真之 <mtane0412@mtane0412MacbookPro.local>
2020-01-21 23:50:14 +0900

    fix http to https

--- Casks/dmm-game-player.rb
+++ Casks/dmm-game-player.rb
@@ -2,9 +2,9 @@ cask 'dmm-game-player' do
   version :latest
   sha256 :no_check
 
-  url "http://dl-gameplayer.dmm.com/admin/mac/installer/DMMGamePlayerSetup.dmg"
+  url "https://dl-gameplayer.dmm.com/admin/mac/installer/DMMGamePlayerSetup.dmg"
   name 'DMMGamePlayer'
-  homepage 'http://www.dmm.com/netgame/top/guide/playerguide_html/=/ch_navi=/'
+  homepage 'https://www.dmm.com/netgame/top/guide/playerguide_html/=/ch_navi=/'
 
   app 'DMMGamePlayer.app'
 end
